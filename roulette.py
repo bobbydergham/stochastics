1c900359ed654dbbf40b0768e5e6f95b4d717d63
bobbydergham <43698628+bobbydergham@users.noreply.github.com>
2019-06-22 15:46:55 -0700

    additions

--- roulette.py
+++ roulette.py
@@ -1,13 +1,4 @@
-import mysql.connector
+import random
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "admin",
-    port = 3306,
-    database = "testdb",
-    auth_plugin='mysql_native_password'
-)
-
-mycursor = mydb.cursor()
-
+class Roulette:
+    
